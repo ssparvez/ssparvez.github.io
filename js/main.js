b095090f099b727382863b66d93ec06591e4620c
ShahPar <shhrrparvez@gmail.com>
2017-03-29 21:28:44 -0400

    fixed nav links

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
 var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
 var color2 = "rgb("+r2+","+g2+","+b2+")";
 
-$('#gradient').css({
+$('body').css({
    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
     background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
   
