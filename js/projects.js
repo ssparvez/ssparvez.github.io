c5b672bf51666ab4239c6580b63b812ffccc9d26
ShahPar <shhrrparvez@gmail.com>
2018-01-16 14:26:54 -0500

    Update to Vue.js

--- js/projects.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("projects", ['ngAnimate']);
-console.log("yo wat up");
-
-app.controller('controlla', function($scope, $http) {
-    $scope.items= [];
-    $http.get('data/projects.json').then(function(response) {
-        $scope.items = response.data;
-    });
-});
